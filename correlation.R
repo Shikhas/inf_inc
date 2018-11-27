c5419878b7d222d450c16fe40ca454098984c511
Saransh-git <kumar.saransh@gmail.com>
2018-11-27 06:30:03 -0600

    scatterplot matrices

--- correlation.R
+++ correlation.R
@@ -1,4 +1,7 @@
 ############################################################################################################################
 # scatter plot and correlation matrix
-
-
+import::here(grwth_county, .from="data_preparation.R")
+pairs(~income + sex_ratio + pct_empd + pct_bpl, data=grwth_county)
+pairs(~income + pct_occ01 + pct_occ02 + pct_occ03 + pct_occ04, data=grwth_county)
+pairs(~income + pct_cow01 + pct_cow02 + pct_cow03 + pct_cow04, data=grwth_county)
+pairs(~income + pct_lessthan_highgrad + pct_highschool_grad + pct_bachdeg_or_highr, data=grwth_county)
