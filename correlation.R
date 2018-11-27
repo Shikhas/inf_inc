c9f7bf96ecf040bdc140e281a8593eaba42b29ec
Shikhas <shikhasaxena7feb@gmail.com>
2018-11-27 06:34:20 -0600

    correlation plot

--- correlation.R
+++ correlation.R
@@ -1,4 +1,6 @@
 ############################################################################################################################
 # scatter plot and correlation matrix
 
-
+grwth_county_numeric<- grwth_county[c(3:ncol(grwth_county))]
+p1 <- cor(grwth_county_numeric)
+corrplot(p1 , method="color", type ='lower', tl.col="black")
